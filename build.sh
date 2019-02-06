3f2e6ef9d8680942f54c718bf4918ccded6eeeae
Jonathan Goldfarb <jgoldfar@gmail.com>
2019-02-06 15:14:42 -0500

    Fixup Alpine & Debian builds

--- build.sh
+++ build.sh
@@ -2,14 +2,16 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+# Debian + Maxima
 DOCKER_REPO_BASE="maxima-docker"
 IMG_TAG=debian-latest
 
 docker build -f Dockerfile.debian -t ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG} .
-#docker push ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}
+docker push ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}
 
+# Alpine + Maxima
 DOCKER_REPO_BASE="maxima-docker"
 IMG_TAG=alpine-latest
 
 docker build -f Dockerfile.alpine -t ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG} .
-#docker push ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}
+docker push ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}
